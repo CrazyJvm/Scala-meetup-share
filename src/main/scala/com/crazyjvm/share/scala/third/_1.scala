f95f1ba6d596d0f916b4bacc97197c645bb0f829
CrazyJvm <crazyjvm@gmail.com>
2014-06-10 17:04:55 +0800

    rename class name of _1

--- src/main/scala/com/crazyjvm/share/scala/third/_1.scala
+++ src/main/scala/com/crazyjvm/share/scala/third/_1.scala
@@ -3,7 +3,7 @@ package com.crazyjvm.share.scala.third
 /**
  * Created by chenchao on 14-6-10.
  */
-class _1 extends App{
+class ByNameDemo extends App{
   var assertionsEnabled = true
   def myAssert(predicate: => Boolean) =
     if (assertionsEnabled && !predicate)
