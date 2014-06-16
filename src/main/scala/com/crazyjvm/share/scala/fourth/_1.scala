3a45a287dc75a7e73b99c91da57903f4d6804d15
CrazyJvm <crazyjvm@gmail.com>
2014-06-16 09:25:16 +0800

    put implicit conversion into an object

--- src/main/scala/com/crazyjvm/share/scala/fourth/_1.scala
+++ src/main/scala/com/crazyjvm/share/scala/fourth/_1.scala
@@ -7,12 +7,15 @@ import scala.io.Source
  * Created by chenchao on 14-6-15.
  */
 object _1 {
-
-  implicit def file2RichFile(file : File) = new RichFile(file)
-
+  import ImplicitContext1._
+  //put your code here and you can convert from File to RichFile directly
 }
 
 class RichFile(val file : File){
   def read = Source.fromFile(file.getPath).mkString
+}
+
+object ImplicitContext1 {
+  implicit def file2RichFile(file : File) = new RichFile(file)
 }
 
---
+++ src/main/scala/com/crazyjvm/share/scala/fourth/_2.scala
@@ -0,0 +1,8 @@
+package com.crazyjvm.share.scala.fourth
+
+/**
+ * Created by chenchao on 14-6-15.
+ */
+object _2 {
+
+}
